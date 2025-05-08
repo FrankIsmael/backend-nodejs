4f8c00a111de2cbfdfc24deabef1658a0a62b3f3
Ismael <ismaelfcom93@gmail.com>
2025-05-08 00:49:51 -0600

    feat: remove robot emoji from greeting message in hello function

--- src/functions/hello/hello.ts
+++ src/functions/hello/hello.ts
@@ -8,7 +8,7 @@ export const lambdaHandler = async (
     body: JSON.stringify(
       {
         message:
-          'Hello World from Serverless with TypeScript! By 🤖Ismael FM with 💚🚀',
+          'Hello World from Serverless with TypeScript! By Ismael FM with 💚🚀',
         input: event,
       },
       null,
