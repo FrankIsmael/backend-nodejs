b177e9282356e75fc67cd4ad5c8d8e40a96c257e
Ismael <ismaelfcom93@gmail.com>
2025-05-12 09:30:53 -0600

    feat: update log message in lambdaHandler function to include rocket emoji

--- src/functions/hello/hello.ts
+++ src/functions/hello/hello.ts
@@ -3,7 +3,7 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 export const lambdaHandler = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
-  console.log('Hello this is a test log');
+  console.log('Hello this is a test log 🚀');
   return {
     statusCode: 200,
     body: JSON.stringify(
